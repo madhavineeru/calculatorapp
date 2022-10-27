90b2d67db7724db44e7db3b5b2f99d2b28f4c904
madhavi <83462607+madhavineeru@users.noreply.github.com>
2022-10-27 21:14:46 +0530

    Added changed files

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let InputElementValue = document.getElementById("inputValues");
-
+//display values
 function display(val){
     InputElementValue.value+=val;
 }
-
+//calculate the given numbers
 function equate(){
     let clickedValue = InputElementValue.value;
     let equatedValue = eval(clickedValue);
     InputElementValue.value = equatedValue;
 }
-
+//clear calculation
 function clr(){
     InputElementValue.value="";
 }
